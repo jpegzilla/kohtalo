e6f5c9c3d43a3044a21778e190d29ce755918a2f
jpegzilla <eris@jpegzilla.com>
2020-01-02 03:02:04 -0500

    first draft of dice roll functionality

--- index.js
+++ index.js
@@ -9,23 +9,67 @@ const opts = {
   },
   connection: { cluster: "aws", reconnect: true },
   identity: {
-    username: "username",
-    // token: twitchapps.com/tmi
-    password: "oauth token"
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
   },
   channels: [channelName]
 };
 
+console.log(channelName);
+
 const client = new tmi.client(opts);
 
-client.connect();
+try {
+  client.connect().catch(console.log);
+} catch (err) {
+  console.log(err);
+}
+
+const rand = (min, max) => Math.round(min + Math.random() * (max - min));
 
 client.on("connected", (add, port) => {
   client.action(channelName, "bot is connected!");
 });
 
 client.on("chat", (channel, user, message, self) => {
-  if (message == "!roll") {
-    client.action(channelName, "rolling!");
+  if (message.startsWith("!roll")) {
+    const tokens = message.split(" ");
+    const parts = {
+      command: tokens[0],
+      amount: {
+        quant: parseInt(tokens[1].split("d")[0]),
+        range: parseInt(tokens[1].split("d")[1])
+      }
+    };
+
+    // minimal error handling:
+    // syntax: (Command)(quantity of random numbers needed: number)d(range: number)
+    const syntaxHint = "!roll (quantity)d(range)";
+
+    if (tokens.length > 2)
+      return client.action(
+        channelName,
+        `invalid command (too many params)! syntax: ${syntaxHint}`
+      );
+
+    if (
+      typeof parts.amount.quant !== "number" ||
+      typeof parts.amount.range !== "number"
+    )
+      return client.action(
+        channelName,
+        `invalid command (params must be numeric)! syntax: ${syntaxHint}`
+      );
+
+    let acc = 0;
+
+    for (let i = 0; i < parts.amount.quant; i++) {
+      acc += rand(1, parts.amount.range);
+    }
+
+    client.action(
+      channelName,
+      `rolled ${parts.amount.quant}d${parts.amount.range}. result: ${acc}!`
+    );
   }
 });
